5287760a10158d4cb71f5e34fa2bb0a72271c3b0
Neerajsharma-2004 <140461376+Neerajsharma-2004@users.noreply.github.com>
2025-07-08 21:56:47 +0530

    Update favorites.js

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -112,8 +112,7 @@ const systemApps = [
     label: "Settings",
     title: "Settings",
     icon: "/assets/icons/setting.png",
-    image: "/assets/images/settings.jpg",
-    description: "Her preferences and quirks.",
+    description: "idk how to make ts",
   },
   {
     label: "Lock",
